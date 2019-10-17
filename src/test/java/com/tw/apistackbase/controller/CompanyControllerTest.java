88daa8a32e42ab631468d3d76196df06ab46436e
BUGARJA <bugarinjm.93@gmail.com>
2019-10-17 17:52:19 +0800

    [JAE&REV]: added test for update method

--- src/test/java/com/tw/apistackbase/controller/CompanyControllerTest.java
+++ src/test/java/com/tw/apistackbase/controller/CompanyControllerTest.java
@@ -3,6 +3,7 @@ package com.tw.apistackbase.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tw.apistackbase.core.Company;
+import com.tw.apistackbase.repository.CompanyRepository;
 import com.tw.apistackbase.service.CompanyService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,11 +18,12 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -30,6 +32,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class CompanyControllerTest {
 
     Iterable<Company> companies;
+
+    private Company company;
+
+    @MockBean
+    private CompanyRepository companyRepository;
 
     @MockBean
     private CompanyService companyService;
@@ -42,7 +49,7 @@ class CompanyControllerTest {
 
     @Test
     void should_return_status_created_when_company_is_created() throws Exception {
-        Company company = new Company();
+        company = new Company();
         ResultActions result = mvc.perform(post("/companies")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(company)));
@@ -53,6 +60,15 @@ class CompanyControllerTest {
     void should_return_all_companies() throws Exception {
         ResultActions result = mvc.perform(get("/companies/all")
                 .contentType(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isOk());
+    }
+
+    @Test
+    void should_return_ok_when_company_is_updated() throws Exception {
+        when(companyService.isUpdated(any())).thenReturn(true);
+        ResultActions result = mvc.perform(put("/companies")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(company)));
         result.andExpect(status().isOk());
     }
 }
